74e5ccdebb7b947fdd994e389b6242177503db3f
gmurugosa <gmurugosa@gmail.com>
2023-12-05 16:17:05 -0300

    first comit

---
+++ api_to_redshift.py
@@ -0,0 +1,8 @@
+def my_function():
+    # Your program logic goes here
+    print("Hello, this is my program!")
+
+if __name__ == "__main__":
+    # This code will run when the script is executed
+    my_function()  # Call your function or write your code here
+    
